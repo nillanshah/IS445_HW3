ab1b537dc88fe134cd601a93d0d32ee76af48f1c
Nillan Shah <nillanshah@gmail.com>
2020-03-09 17:56:50 -0700

    initial commit

---
+++ js/test.js
@@ -0,0 +1,11 @@
+console.log("Hello from JavaScript!");
+const temp = prompt("Enter test");
+console.log(temp[0]);
+let start;
+let end;
+let step;
+
+function myFunction() {
+  var number = "1234";
+  document.getElementById("myText").innerHTML = number;
+}
