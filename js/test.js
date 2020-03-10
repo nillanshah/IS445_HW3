b7704cafb8717e8921ac3ea98c288b7e90595320
Nillan Shah <nillanshah@gmail.com>
2020-03-09 18:24:18 -0700

    Added a lot of JS and HTML

--- js/test.js
+++ js/test.js
@@ -1,11 +1,23 @@
-console.log("Hello from JavaScript!");
-const temp = prompt("Enter test");
-console.log(temp[0]);
-let start;
-let end;
-let step;
+//const start = Number(prompt("Enter start"));
+//const end = Number(prompt("Enter end"));
+const step = Number(prompt("Enter step"));
 
-function myFunction() {
-  var number = "1234";
-  document.getElementById("myText").innerHTML = number;
+function getRange(start, end, step){
+  arr = [];
+  
+
+
+
+}
+
+function validInput(){
+
+  if (!(step < 0 || step > 0)){
+    var output = "Invalid integer input, please reload and try again";
+    document.getElementById("myText").innerHTML = output;
+  } else {
+    var range = getRange()
+  }
+  
+
 }
